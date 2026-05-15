5c667d7edc77309aaa63e7dda917a978fff31c6b
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-05-15 14:13:10 +0530

    Optimization Done

--- ipconfig/main.py
+++ ipconfig/main.py
@@ -1,14 +1,14 @@
 def ipClassFinder(ipAdress):
     ipPrefix=int(ipAdress.split(".")[0])
-    if ipPrefix>=0 and ipPrefix<=127:
+    if ipPrefix in range(0,127):
         print("Class A IP Adress")
-    elif ipPrefix>=128 and ipPrefix<=191:
+    elif ipPrefix in range(128,191):
         print("Class B IP Adress")
-    elif ipPrefix>=192 and ipPrefix<=223:
+    elif ipPrefix in range(192,223):
         print("Class C IP Adress")
-    elif ipPrefix>=224 and ipPrefix<=239:
+    elif ipPrefix in range(224,239):
         print("Class D IP Adress")
-    elif ipPrefix>=240 and ipPrefix<=255:
+    elif ipPrefix in range(240,255):
         print("Class E IP Adress")
     else:
         print("Invalid IP Adress")
